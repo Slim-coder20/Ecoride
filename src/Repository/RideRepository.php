107dcea07750fac988a60c83f2496c81abfa0ad8
slim <slimabida21@gmail.com>
2025-03-18 23:44:37 +0100

    correction du ReideRepositiory dans la méthode findRidesByCriteria

--- src/Repository/RideRepository.php
+++ src/Repository/RideRepository.php
@@ -16,29 +16,34 @@ class RideRepository extends ServiceEntityRepository
         parent::__construct($registry, Ride::class);
     }
 
-    
-   /**
-    * Recherche des trajets correspondant aux critères de depart et de destination et de la date
-
-    * @param string  $departure
-    * @param string  $destination
-    * @param \DateTimeInterface $date
-    * @return Ride[]
-    */
+    /**
+     * Recherche des trajets correspondant aux critères de depart et de destination et de la date
+     *
+     * @param string $departure
+     * @param string $destination
+     * @param \DateTimeInterface $date
+     * @return Ride[]
+     */
     public function findRidesByCriteria(string $departure, string $destination, \DateTimeInterface $date): array
     {
+        // Début de la journée
+        $startDate = (clone $date)->setTime(0, 0, 0);
+        // Fin de la journée
+        $endDate = (clone $date)->setTime(23, 59, 59);
+
         return $this->createQueryBuilder('r')
-            ->andWhere('r.departure = :departure')
-            ->andWhere('r.destination = :destination')
-            ->andWhere('r.date = :date')
+            ->andWhere('r.departureCity = :departure')
+            ->andWhere('r.destinationCity = :destination')
+            ->andWhere('r.departureDate BETWEEN :startDate AND :endDate') // Comparaison sur la plage de dates
+            ->andWhere('r.remainingSeats > 0') // Trajets avec des places disponibles
             ->setParameter('departure', $departure)
             ->setParameter('destination', $destination)
-            ->setParameter('date', $date->format('Y-m-d'))
-            ->orderBy('r.departureDate', 'ASC')
+            ->setParameter('startDate', $startDate)
+            ->setParameter('endDate', $endDate)
+            ->orderBy('r.departureDate', 'ASC') // Trier par date de départ
             ->getQuery()
             ->getResult();
     }
-
 
     /**
      * Recherche des trajets les plus proches de la date de départ
@@ -56,23 +61,6 @@ class RideRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
