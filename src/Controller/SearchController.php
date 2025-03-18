affaae1a46679efa77a830247ee0e79a064f1e14
slim <slimabida21@gmail.com>
2025-03-18 17:49:01 +0100

    correction dans la classe SearchType

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -8,8 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoicesType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -43,14 +41,13 @@ class SearchType extends AbstractType
                     'placeholder' => 'Saisir le nombre de passagers'
                 ]
             ])
-            ->add('smokers', ChoicesType::class,[
+            ->add('smokers', ChoiceType::class, [
                 'choices' => [
-                    'Indifféren' => 'indifferent',
+                    'Indifférent' => 'indifferent',
                     'Oui' => 'yes',
                     'Non' => 'no'
                 ],
                 'label' => 'Fumeurs',
-
             ])
             ->add('animals', ChoiceType::class, [
                 'choices' => [
@@ -60,7 +57,6 @@ class SearchType extends AbstractType
                 ],
                 'label' => 'Animaux',
             ])
-
             ->add('fuelType', ChoiceType::class, [
                 'choices' => [
                     'Tous' => 'all',
@@ -80,15 +76,12 @@ class SearchType extends AbstractType
                 ],
                 'label' => 'Type de véhicule',
             ])
-
             ->add('search', SubmitType::class, [
                 'label' => 'Rechercher',
             ])
             ->add('reset', SubmitType::class, [
                 'label' => 'Réinitialiser',
             ]);
-
-        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
