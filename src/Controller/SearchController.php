14d3ae873c2a2dee406b4d435a1ab88bf906c9e3
slim <slimabida21@gmail.com>
2025-03-18 17:21:36 +0100

    création de nouveau controller pour le menu recherche de trajet et création du formulaire et du template pour la vue utilisateur et rajout du path dans la base.html.twig

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/rechercher', name: 'app_search')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // traitement des données ici 
+            $data = $form->getData();
+            // effectuer une recherche dans la base de données et passez les résultats à la vue
+        
+        }
+        
+            return $this->render('search/search.html.twig', [
+            'controller_name' => 'SearchController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/SearchType.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoicesType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('departure', TextType::class, [
+                'label' => 'Départ',
+                'attr' => [
+                    'placeholder' => 'Saisir une ville de départ'
+                ]
+            ])
+            ->add('destination', TextType::class, [
+                'label' => 'Destination',
+                'attr' => [
+                    'placeholder' => 'Saisir une ville de destination'
+                ]
+            ])
+            ->add('date', DateType::class, [
+                'label' => 'Date de départ',
+                'widget' => 'single_text',
+                'attr' => [
+                    'placeholder' => 'Sélectionner une date de départ'
+                ]
+            ])
+            ->add('passengers', IntegerType::class, [
+                'label' => 'Nombre de passagers',
+                'attr' => [
+                    'placeholder' => 'Saisir le nombre de passagers'
+                ]
+            ])
+            ->add('smokers', ChoicesType::class,[
+                'choices' => [
+                    'Indifféren' => 'indifferent',
+                    'Oui' => 'yes',
+                    'Non' => 'no'
+                ],
+                'label' => 'Fumeurs',
+
+            ])
+            ->add('animals', ChoiceType::class, [
+                'choices' => [
+                    'Indifférent' => 'indifferent',
+                    'Oui' => 'yes',
+                    'Non' => 'no',
+                ],
+                'label' => 'Animaux',
+            ])
+
+            ->add('fuelType', ChoiceType::class, [
+                'choices' => [
+                    'Tous' => 'all',
+                    'Essence' => 'petrol',
+                    'Diesel' => 'diesel',
+                    'Électrique' => 'electric',
+                    'Hybride' => 'hybrid',
+                ],
+                'label' => 'Type de carburant',
+            ])
+            ->add('vehicleType', ChoiceType::class, [
+                'choices' => [
+                    'Indifférent' => 'indifferent',
+                    'Voiture' => 'car',
+                    'Moto' => 'motorcycle',
+                    'Vélo' => 'bicycle',
+                ],
+                'label' => 'Type de véhicule',
+            ])
+
+            ->add('search', SubmitType::class, [
+                'label' => 'Rechercher',
+            ])
+            ->add('reset', SubmitType::class, [
+                'label' => 'Réinitialiser',
+            ]);
+
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
