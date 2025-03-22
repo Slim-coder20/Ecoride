118886c1429e512509dccab4d08624c8e2027397
slim <slimabida21@gmail.com>
2025-03-22 15:23:25 +0100

    création du Controller CovoiturageController et de son template covoiturage.html.twig

---
+++ src/Controller/CovoiturageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CovoiturageController extends AbstractController
+{
+    #[Route('/covoiturage', name: 'app_covoiturage')]
+    public function index(): Response
+    {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return $this->render('covoiturage/covoiturage.html.twig', [
+            'controller_name' => 'CovoiturageController',
+        ]);
+    }
+}
