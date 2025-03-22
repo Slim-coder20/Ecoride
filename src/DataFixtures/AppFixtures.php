493936f5d30528e59b4931cea04cd2e13372fa16
slim <slimabida21@gmail.com>
2025-03-22 20:01:17 +0100

    création de AppFixture pour insérer des données de trajet dans la table Trajet dans la BDD test OK

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Trajet;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,36 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // création d'un utilisateur chauffeur pour les tests // 
+
+        $chauffeur = new User();
+        $chauffeur->setPseudo('chauffeur');
+        $chauffeur->setEmail('JeanChauffeur'); 
+        $chauffeur->setPassword('password');
+        $chauffeur->setIsChauffeur(true);
+        $chauffeur->setBrand('Peugeot');
+        $chauffeur->setModel('208');
+        $chauffeur->setLicensePlate('AB-123-CD');
+        $chauffeur->setSeat(4);
+        
+        $manager->persist($chauffeur);
+
+        // Création de plusieurs trajets pour les tests // 
+
+        for($i = 0; $i < 10; $i++){
+            $trajet = new Trajet();
+            $trajet->setDepart('Paris');
+            $trajet->setArrivee('Lyon');
+            $trajet->setDateDepart(new \DateTime("+{$i} days 08:00"));
+            $trajet->setDateArrivee(new \DateTime("+{$i} days 12:00"));
+            $trajet->setPrix(20 + $i * 5);
+            $trajet->setPlacesRestantes(3 - ($i % 3));
+            $trajet->setChauffeur($chauffeur);
+
+            $manager->persist($trajet);
+        }
+
+
 
         $manager->flush();
     }
