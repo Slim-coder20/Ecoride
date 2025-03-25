ed7e9ca20ffe44b97a4ef68b3bc989a55dacab99
slim <slimabida21@gmail.com>
2025-03-25 09:32:19 +0100

    correction du UserType fomr mis a jour avec le controller RegisrationController

---
+++ src/Form/UserType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'label' => "Nom d'utilisateur",
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez saisir un pseudo.']),
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => "Adresse email",
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez saisir une adresse email.']),
+                ]
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => "Mot de passe",
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez saisir un mot de passe.']),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
+                        'max' => 4096
+                    ])
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
