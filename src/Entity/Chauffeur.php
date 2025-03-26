23a122014db5932f23eb52792aa2f08382536c18
slim <slimabida21@gmail.com>
2025-03-26 20:54:31 +0100

    mise a jour dans l'entité chauffeur avec l'ajout de la relation OneToMany avec l'entité Vehicule

--- src/Entity/Chauffeur.php
+++ src/Entity/Chauffeur.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ChauffeurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ChauffeurRepository::class)]
@@ -20,6 +22,14 @@ class Chauffeur
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
+    #[ORM\OneToMany(mappedBy: 'chauffeur', targetEntity: Vehicule::class, orphanRemoval: true, cascade: ['persist'])]
+    private Collection $vehicules;
+
+    public function __construct()
+    {
+        $this->vehicules = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -33,7 +43,6 @@ class Chauffeur
     public function setPreferences(array $preferences): static
     {
         $this->preferences = $preferences;
-
         return $this;
     }
 
@@ -45,7 +54,33 @@ class Chauffeur
     public function setUser(User $user): static
     {
         $this->user = $user;
+        return $this;
+    }
 
+    /**
+     * @return Collection<int, Vehicule>
+     */
+    public function getVehicules(): Collection
+    {
+        return $this->vehicules;
+    }
+
+    public function addVehicule(Vehicule $vehicule): static
+    {
+        if (!$this->vehicules->contains($vehicule)) {
+            $this->vehicules[] = $vehicule;
+            $vehicule->setChauffeur($this);
+        }
+        return $this;
+    }
+
+    public function removeVehicule(Vehicule $vehicule): static
+    {
+        if ($this->vehicules->removeElement($vehicule)) {
+            if ($vehicule->getChauffeur() === $this) {
+                $vehicule->setChauffeur(null);
+            }
+        }
         return $this;
     }
 }
