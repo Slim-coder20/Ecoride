22005e6e03abe7c1ed0260b2ebb6844c87573612
slim <slimabida21@gmail.com>
2025-03-26 16:27:51 +0100

    modification dans l'entité chauffeur test BDD OK

--- src/Entity/Chauffeur.php
+++ src/Entity/Chauffeur.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\ChauffeurRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ChauffeurRepository::class)]
@@ -14,35 +13,8 @@ class Chauffeur
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $licensePlate = null;
-
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $registrationDate = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $model = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $brand = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $color = null;
-
-    #[ORM\Column]
-    private ?int $seat = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $energie = null;
-
-    #[ORM\Column]
-    private ?bool $ecologique = null;
-
     #[ORM\Column]
     private array $preferences = [];
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $photo = null;
 
     #[ORM\OneToOne(inversedBy: 'chauffeur', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
@@ -53,94 +25,6 @@ class Chauffeur
         return $this->id;
     }
 
-    public function getLicensePlate(): ?string
-    {
-        return $this->licensePlate;
-    }
-
-    public function setLicensePlate(string $licensePlate): static
-    {
-        $this->licensePlate = $licensePlate;
-        return $this;
-    }
-
-    public function getRegistrationDate(): ?\DateTimeInterface
-    {
-        return $this->registrationDate;
-    }
-
-    public function setRegistrationDate(\DateTimeInterface $registrationDate): static
-    {
-        $this->registrationDate = $registrationDate;
-        return $this;
-    }
-
-    public function getModel(): ?string
-    {
-        return $this->model;
-    }
-
-    public function setModel(string $model): static
-    {
-        $this->model = $model;
-        return $this;
-    }
-
-    public function getBrand(): ?string
-    {
-        return $this->brand;
-    }
-
-    public function setBrand(string $brand): static
-    {
-        $this->brand = $brand;
-        return $this;
-    }
-
-    public function getColor(): ?string
-    {
-        return $this->color;
-    }
-
-    public function setColor(string $color): static
-    {
-        $this->color = $color;
-        return $this;
-    }
-
-    public function getSeat(): ?int
-    {
-        return $this->seat;
-    }
-
-    public function setSeat(int $seat): static
-    {
-        $this->seat = $seat;
-        return $this;
-    }
-
-    public function getEnergie(): ?string
-    {
-        return $this->energie;
-    }
-
-    public function setEnergie(string $energie): static
-    {
-        $this->energie = $energie;
-        return $this;
-    }
-
-    public function isEcologique(): ?bool
-    {
-        return $this->ecologique;
-    }
-
-    public function setEcologique(bool $ecologique): static
-    {
-        $this->ecologique = $ecologique;
-        return $this;
-    }
-
     public function getPreferences(): array
     {
         return $this->preferences;
@@ -149,17 +33,7 @@ class Chauffeur
     public function setPreferences(array $preferences): static
     {
         $this->preferences = $preferences;
-        return $this;
-    }
 
-    public function getPhoto(): ?string
-    {
-        return $this->photo;
-    }
-
-    public function setPhoto(?string $photo): static
-    {
-        $this->photo = $photo;
         return $this;
     }
 
@@ -171,6 +45,7 @@ class Chauffeur
     public function setUser(User $user): static
     {
         $this->user = $user;
+
         return $this;
     }
 }
