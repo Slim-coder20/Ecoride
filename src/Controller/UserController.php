20074e58da9b9b377418ce93d56470496fb71517
slim <slimabida21@gmail.com>
2025-03-26 10:47:45 +0100

    mise en place de l'espace abonnée et création de la vue devenir chauffeur avec le formulaire test OK

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,34 +18,56 @@ final class UserController extends AbstractController
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
 
+        // Cette condition permet de rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté // 
         if (!$user) {
             throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
         }
 
-        // Si l'utilisateur n'a pas encore de profil chauffeur, on le crée
-        $chauffeur = $user->getChauffeur();
-        if (!$chauffeur) {
+        return $this->render('user/dashboard.html.twig', [
+            'user' => $user,
+        ]);
+
+        
+    }
+
+    // création de la route pour devenir chauffeur //
+    
+    #[Route('/chauffeur/devenir', name: 'app_become_driver')]
+    public function becomeDriver(Request $request, EntityManagerInterface $em): Response
+    {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        // Cette condition permet de rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté // 
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
+        }
+
+        // créations de l'objet chauffeur si inexistant //
+
+        if (!$user->getChauffeur()) {
             $chauffeur = new Chauffeur();
             $chauffeur->setUser($user);
             $user->setChauffeur($chauffeur);
             $em->persist($chauffeur);
+        } else {
+            $chauffeur = $user->getChauffeur();
         }
 
-        // Création et traitement du formulaire
         $form = $this->createForm(DriverInfoType::class, $chauffeur);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if($form->isSubmitted() && $form->isValid()) {
             $em->flush();
-
-            $this->addFlash('success', 'Vos informations de chauffeur ont bien été mises à jour ✅');
-
+            $this->addFlash('success', 'Vos infos chauffeur ont bien été enregitrées.');
             return $this->redirectToRoute('app_user_dashboard');
         }
 
-        return $this->render('user/dashboard.html.twig', [
-            'user' => $user,
+        return $this->render('user/become_driver.html.twig',[
             'form' => $form->createView(),
         ]);
+
+        
+        
     }
 }
