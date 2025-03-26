84527649a910034ad8c3e85f0e7459d81d8b2f7d
slim <slimabida21@gmail.com>
2025-03-27 00:11:46 +0100

    correction dans les injestions du formulaire VehiculeType

--- src/Form/VehiculeType.php
+++ src/Form/VehiculeType.php
@@ -2,12 +2,10 @@
 
 namespace App\Form;
 
-use App\Entity\Chauffeur;
 use App\Entity\Vehicule;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
