96a2de069e16d4dbe1b3044d08e8fda30ea70955
slim <slimabida21@gmail.com>
2025-04-01 18:36:35 +0200

    mise en place de la paticiation du User au trajet selectionner via le fomuliare de recherche dans le le participationController  et l'afficher dans le tableau d'historique depuis l'espace aboonée

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\User;
+
 use App\Entity\Participation;
-use App\Repository\ParticipationRepository;
+use App\Entity\Trajet;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +14,6 @@ class ParticipationController extends AbstractController
     #[Route('/participation/{id}/valider', name: 'valider_participation', methods: ['POST'])]
     public function valider(Participation $participation, EntityManagerInterface $em): Response
     {
-        // Vérification sécurité
         if ($participation->getUser() !== $this->getUser()) {
             throw $this->createAccessDeniedException("Accès refusé.");
         }
@@ -37,15 +36,11 @@ class ParticipationController extends AbstractController
             throw $this->createAccessDeniedException("Ce trajet ne vous appartient pas.");
         }
 
-        // Mise à jour du statut et des données liées
         $participation->setStatus('Annulée');
         $participation->setConfirmation(false);
 
-        // Crédit remboursé
-        /** @var \App\Entity\User $user */
         $user->setCredits($user->getCredits() + 1);
 
-        // Libérer la place
         $trajet = $participation->getTrajet();
         $trajet->setPlacesRestantes($trajet->getPlacesRestantes() + 1);
 
@@ -55,10 +50,43 @@ class ParticipationController extends AbstractController
         return $this->redirectToRoute('app_user_dashboard');
     }
 
-    
+    #[Route('/participer/{id}', name: 'app_participer', methods: ['POST'])]
+    public function participer(Trajet $trajet, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
 
+        if (!$user) {
+            $this->addFlash('error', 'Vous devez être connecté pour participer à un trajet.');
+            return $this->redirectToRoute('app_login');
+        }
 
+        // Empêche les participations en double
+        foreach ($user->getParticipations() as $participation) {
+            if ($participation->getTrajet() === $trajet) {
+                $this->addFlash('warning', 'Vous participez déjà à ce trajet.');
+                return $this->redirectToRoute('app_user_dashboard');
+            }
+        }
 
+        // Vérifie qu'il reste des places
+        if ($trajet->getPlacesRestantes() <= 0) {
+            $this->addFlash('danger', 'Aucune place restante pour ce trajet.');
+            return $this->redirectToRoute('app_user_dashboard');
+        }
 
+        $participation = new Participation();
+        $participation->setUser($user);
+        $participation->setTrajet($trajet);
+        $participation->setStatus('En attente');
+        $participation->setConfirmation(false);
 
+        $trajet->setPlacesRestantes($trajet->getPlacesRestantes() - 1);
+        $user->setCredits($user->getCredits() - 1);
+
+        $em->persist($participation);
+        $em->flush();
+
+        $this->addFlash('success', 'Vous avez rejoint le trajet.');
+        return $this->redirectToRoute('app_user_dashboard');
+    }
 }
