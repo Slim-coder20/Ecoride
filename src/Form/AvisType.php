82c5d32ac56fb56d8fd7e9a6f6af3b312cb483c9
slim <slimabida21@gmail.com>
2025-04-03 23:53:12 +0200

    création du formuliare AvisType

---
+++ src/Form/AvisType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avis;
+use App\Entity\Trajet;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AvisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('note', IntegerType::class, [
+                'label' => 'Note (1 à 5)',
+                'attr' => [
+                    'min' => 1,
+                    'max' => 5,
+                ],
+            ])
+            ->add('commentaire',TextareaType::class, [
+                'label' => 'Commentaire',
+                'attr' => [
+                    'placeholder' => 'Votre commentaire',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
