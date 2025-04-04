1c7ff45db9c658aa27f273244c5d1240340189f0
slim <slimabida21@gmail.com>
2025-04-04 10:53:40 +0200

    MAJ dans le AvisType

--- src/Form/AvisType.php
+++ src/Form/AvisType.php
@@ -3,9 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Avis;
-use App\Entity\Trajet;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
