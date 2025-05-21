9d422bf8567a3234cff245d833c1cf065ce17790
slim <slimabida21@gmail.com>
2025-05-21 23:03:46 +0200

    suppression de la route reset password du loginController

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -48,54 +48,8 @@ final class LoginController extends AbstractController
     }
 
     
-    /**
-     * Cette route nous permet de rénitialiser le mot de passe de l'utilisateur 
-     * en cas d'oubli
-    */
-    #[Route('/motdepasseoublie', name: 'app_forgotten_password')]
-    public function forgottenPassword(Request $request, UserRepository $userRepository): Response
     
-    {
-        // Initialiser le formulaire de réinitialisation du mot de passe
-        $form = $this->createform(ResetPasswordRequestFormType::class);
-        
-        $form ->handleRequest($request);
-        // Vérifier si le formulaire est soumis et valide 
-        if($form->isSubmitted() && $form->isValid())
-        {
-        // le fomrulaire est envoyé et valide // 
-        // On va chercher l''utilisateur en base de données // 
-        $user = $userRepository->findOneByEmail($form->get('email')->getData());
-           // On vérifie si on a bien un utilisateur // 
-           if($user)
-           {
-            // On a trouvé un utiisateur // 
-            // ON génère un token de rénitialisation de mot de passe // 
-            
-
-                
-           }
-           //$user = Null; 
-           $this->addFlash('error', 'Un problème est survenu lors de la réinitialisation du mot de passe. Veuillez réessayer.');
-           return $this->redirectToRoute('app_login');
-
-         
-
-        
-        }
 
         
         
-        
-        // Afficher la page de réinitialisation du mot de passe
-        return $this->render('login/forgotten_password.html.twig',[
-            'form' => $form->createView(),
-        ]);
-    }
-
-
-
-
-
-
 }
